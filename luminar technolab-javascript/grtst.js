dbddc4ba329dcc70f066ef06ed8f90a2ea018e6a
emmanualantony007 <antony.manu6@gmail.com>
2021-11-11 09:21:10 +0530

    grtst

--- luminar technolab-javascript/grtst.js
+++ luminar technolab-javascript/grtst.js
@@ -1,11 +1,12 @@
 var a=2
 var b=45
-if(a>b){
-    console.log("a is great")
-}
-else if(a==b){
-    console.log("equal")
-}
-else{
-    console.log("b is greater")
-}
+//if(a>b){
+  //  console.log("a is great")
+//}
+//else if(a==b){
+  //  console.log("equal")
+//}
+//else{
+ //   console.log("b is greater")
+//}
+console.log(a>b?a:b);
